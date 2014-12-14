2023d7848e753fd25693ffa579566340b9e9e28a
Malcolm <malcolm@Malcolms-Mac-Pro.local>
2014-12-14 17:19:09 +0800

    Initial Commit

---
+++ Test5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test5
+//
+//  Created by Malcolm on 14/12/14.
+//  Copyright (c) 2014 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Test5Tests/Test5Tests.m
@@ -0,0 +1,40 @@
+//
+//  Test5Tests.m
+//  Test5Tests
+//
+//  Created by Malcolm on 14/12/14.
+//  Copyright (c) 2014 Malcolm Harrow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface Test5Tests : XCTestCase
+
+@end
+
+@implementation Test5Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
